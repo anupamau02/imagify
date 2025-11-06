15c4404d6f0cbaa1f6291403e38cb61a41bc9765
anupamau02 <uanupama03@gmail.com>
2025-11-06 16:23:07 +0530

    Fix: corrected motion import in BuyCredit.jsx

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,4 +1,4 @@
-import { motion } from "motion/react"
+import { motion } from "framer-motion"
 import { useContext } from "react"
 import { assets, plans } from "../assets/assets"
 import { AppContext } from "../context/AppContext"
